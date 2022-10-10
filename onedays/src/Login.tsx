584aa27b38985606fc661d3d4757f5bf182d47b8
Hakan ÖZER <hakanozer02@gmail.com>
2022-10-10 15:59:22 +0300

    1-Days

--- onedays/src/Login.tsx
+++ onedays/src/Login.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { IUser } from './models/IUser'
 import appTitle, { userPasswordValid } from './util'
 
@@ -49,16 +49,57 @@ function Login() {
     name: 'Erkan',
     surname: 'Bilsin',
     age: 20,
-    status: true
+    status: true,
+    email: 'erkan@mail.com',
+    address: {
+      code: 2022,
+      no: 4,
+      title: "Beşiktaş"
+    }
+  }
+
+  interface IAction {
+    id: number,
+    fncCall: Function
+  }
+
+  const fncSum = ( a: number, b: number ) : number => {
+    return a + b
+  }
+
+  const itemAction: IAction = {
+    id: 0,
+    fncCall: fncSum
+  }
+
+  const sm = itemAction.fncCall(10, 20)
+  console.log("Sum : ", sm );
+  
+  // Use State
+  const [data, setData] = useState('')
+  const sendData = ( ) => {
+    console.log(data);
   }
  
   return (
     <>
         <h1> { appTitle } </h1>
+
+        <input onChange={(evt) => setData(evt.target.value) } />
+        <button onClick={ sendData } >Send</button>
+        <h2> { data } </h2>
+        {  data.split('').map( (item, index) => 
+          <li key={index}> { item } </li>
+        )}
+
         <h2>User Login</h2>
         <p>User Detail</p>
         <div> { user.name } </div>
         <div> { userx.name } </div>
+        <div> { userx.address?.code } </div>
+        { userx.email &&  
+          <div> { userx.email.length } </div>
+        }
         { cities.map( ( item, index ) => 
             <div key={index}><li> { item.toString() } </li></div>
         )}
--- onedays/src/models/IUser.tsx
+++ onedays/src/models/IUser.tsx
@@ -2,5 +2,13 @@ export interface IUser {
     name: string,
     surname: string,
     age: number,
-    status: boolean
+    status: boolean,
+    email?: string,
+    address?: IAddress
+}
+
+export interface IAddress {
+    title: string,
+    no: number,
+    code: number
 }
